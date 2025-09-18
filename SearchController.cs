02f62e83234b0a9eb49b3bff784d7944699a1601
Chris Åkerfeldt Wendel <chga@multinet.se>
2025-09-18 15:24:43 +0200

    fix: Missing namespace

--- SearchController.cs
+++ SearchController.cs
@@ -1,6 +1,6 @@
 ﻿using Manifest.Report.Classes;
 using Manifest.Report.Classes.DBClasses;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using StackExchange.Redis;
